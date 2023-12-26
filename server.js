feecbadfaa2d03c368f4edf6096ec495c20fe7f1
Tushar Jha <tushar.jha@servicenow.com>
2023-12-26 22:41:12 +0530

    DB connect

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+/* remove this 
+// conect database
+MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      console.error('Error connecting to MongoDB:', err);
+      return;
+    }
+  
+    console.log('Connected to MongoDB');
+  );
+
+   */ 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
